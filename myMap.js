bd06eae3820ebc55fb1efc67a00938b667fd7f1f
EasonJackson <pyhanson@gmail.com>
2016-12-22 17:27:43 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -30,16 +30,29 @@ function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AheCiOGSkSuZ-AsuNQoqbbG55QAS74h9R1Oj6pzsWiZBQZifdk7fKX5LaIv3wI07'
     });
-    map_manager.map = map;
-    
+    map_manager.map = map;  
+    window.setInterval(refresh_pokemon(), 1000);
+}
+
+function refresh_pokemon() {
+    // 1. Pushipins (pushpin layer)
+    var layer = new Microsoft.Maps.Layer();
+    var pushpins = [];
     for (var i in map_manager.map_items) {
         var map_item = map_manager.map_items[i];
         var icon_url = 'https://github.com/EasonJackson/Pgo_map/raw/master/'+map_item["pokemon_id"]+'.png';
         var count_down = get_count_down_from_timestamp(map_item["expire"]);
         var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
                                        {title: count_down, 
-                                        icon: icon_url});
-        map.entities.push(pushpin);
+                                        icon: icon_url}); 
+        pushpins.push(pushpin);
     }
-
+    
+    layer.add(pushipin);
+    
+    // 2. Remove old pushpins
+    map.layers.clear();
+    
+    // 3. Add new pushpins
+    map_manager.map.layers.insert(layer);
 }
