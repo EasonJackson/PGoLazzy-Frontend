998e93e5c5a81e89eb30ff29e1de3d999dbf29c0
EasonJackson <pyhanson@gmail.com>
2016-12-22 15:51:10 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -6,6 +6,6 @@ map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
 
 var pushpin 
 
-pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',});
+pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png'});
 
 map.entities.push(pushpin);
