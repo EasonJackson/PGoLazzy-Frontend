2ae8c57f53dd2594051145a081ab6b6c91cc76e4
EasonJackson <pyhanson@gmail.com>
2016-12-22 15:45:54 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -5,6 +5,6 @@ function loadMapScenario() {
     });
     
     var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
-    anchor: new Microsoft.Maps.Point(12, 39) });
+    });
     map.entities.push(pushpin);
 }
