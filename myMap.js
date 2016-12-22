53ff63652423c31275410ff70bc6cfa0e178be9f
EasonJackson <pyhanson@gmail.com>
2016-12-22 16:06:05 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -2,7 +2,7 @@ function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AheCiOGSkSuZ-AsuNQoqbbG55QAS74h9R1Oj6pzsWiZBQZifdk7fKX5LaIv3wI07'
     });
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://raw.githubusercontent.com/EasonJackson/Pgo_map/master/1.png',
         anchor: new Microsoft.Maps.Point(12, 39) });
     map.entities.push(pushpin);
 
