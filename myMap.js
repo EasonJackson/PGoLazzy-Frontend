01ceeb334a444164fcbbae36157fbeb6486b5896
EasonJackson <pyhanson@gmail.com>
2016-12-22 15:39:21 -0500

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: ' AheCiOGSkSuZ-AsuNQoqbbG55QAS74h9R1Oj6pzsWiZBQZifdk7fKX5LaIv3wI07'
+    });
+}
