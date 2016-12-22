34dc2e8890efd30254adf64835ef2fa17ecdca62
EasonJackson <pyhanson@gmail.com>
2016-12-22 16:53:26 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -18,15 +18,27 @@ map_manager.map_items = [
     },
 ]
 
+function get_countdown_from_timestamp(expire) {
+    var now_time = new Date().getTime() / 1000;
+    var time_left = expire - now_time;
+    var second = Math.floor(time_left % 60);
+    var minute = Math.floor(time_left / 60);
+    return minute + ":" + second;
+}
+
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AheCiOGSkSuZ-AsuNQoqbbG55QAS74h9R1Oj6pzsWiZBQZifdk7fKX5LaIv3wI07'
     });
+    map_manager.map = map;
     
     for (var i in map_manager.map_items) {
         var map_item = map_manager.map_items[i];
+        var icon_url = 'https://github.com/EasonJackson/Pgo_map/raw/master/'+map_item["pokemon_id"]+'.png';
+        var count_down = get_count_down_from_timestamp(map_item["expire"]);
         var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
-                                                 { icon: 'https://github.com/EasonJackson/Pgo_map/raw/master/'+map_item["pokemon_id"]+'.png'});
+                                       {title: count_down, 
+                                        icon: icon_url});
         map.entities.push(pushpin);
     }
 
